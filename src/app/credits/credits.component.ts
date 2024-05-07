123f6dc7c28eb53777c656dd6e16c5fd3c246e15
Daniel Fausz <dfausz@ebsco.com>
2024-05-07 00:05:27 -0500

    Theme changes and added build

--- src/app/credits/credits.component.ts
+++ src/app/credits/credits.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-credits',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDialogModule,
+    MatButtonModule
+  ],
   templateUrl: './credits.component.html',
   styleUrl: './credits.component.scss'
 })
 export class CreditsComponent {
-
 }
--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -3,7 +3,8 @@ import { MatChipsModule } from "@angular/material/chips";
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { CreditsComponent } from '../credits/credits.component';
 
 @Component({
   selector: 'app-experience',
@@ -12,11 +13,17 @@ import { MatButtonModule } from '@angular/material/button';
     MatChipsModule, 
     MatDividerModule, 
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule
   ],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.scss'
 })
 export class ExperienceComponent {
 
+  constructor(private dialog: MatDialog) { }
+
+  openDialog() {
+    this.dialog.open(CreditsComponent);
+  }
 }
