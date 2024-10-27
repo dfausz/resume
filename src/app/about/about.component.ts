f1f6ad708daa894bfdd0da86329efdabc3762e16
dfausz <dbfausz@gmail.com>
2024-10-27 00:51:59 -0500

    Lots of resizing work and rework on experiences section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { SocialsComponent } from '../socials/socials.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
-    MatCardModule
+    MatCardModule,
+    SocialsComponent,
+    MatButtonModule
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
